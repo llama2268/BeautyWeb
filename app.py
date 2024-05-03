66115b375951737d4bd5313012472faded4146b3
Ian Urquhart <ianurquhart@dhcp-vl2042-8542.redrover.cornell.edu>
2024-05-03 17:41:33 -0400

    ian changes v1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from db import db
 from flask import Flask, request
 from flask import json
-from db import User, Review
+from db import User, Review, Post, Comment
 
 app = Flask(__name__)
 db_filename = "ithacuts.db"
@@ -9,6 +9,7 @@ db_filename = "ithacuts.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
+app.config["UPLOAD_FOLDER"] = 'uploads'
 
 db.init_app(app)
 with app.app_context():
@@ -42,17 +43,17 @@ def create_user():
     contacts2 = body.get("contacts")
     checkusername = User.query.filter_by(username = username2).first()
     if checkusername is not None:
-        return json.dumps({"error":"Username already exists"}), 400
+        return failure_response("Username already exists")
     if username2 is None:
-        return json.dumps({"error":"Missing Username"}),400
+        return failure_response("Missing username")
     if bio2 is None:
-        return json.dumps({"error":"Missing Bio"}),400
+        return failure_response("Missing bio")
     if contacts2 is None:
-        return json.dumps({"error":"Missing contacts"})
+        return failure_response("Missing contact")
     new_user = User(username = username2,bio=bio2,contacts=contacts2)
 
     if new_user is None:
-        return failure_response("Invalid code and name for course")
+        return failure_response("Invalid user")
     db.session.add(new_user)
     db.session.commit()
     return success_response(new_user.serialize(), 201)
@@ -64,7 +65,7 @@ def get_user(user_id):
     """
     user = User.query.filter_by(id=user_id).first()
     if user is None:
-        return failure_response("Course not found")
+        return failure_response("Course not found", 404)
     return success_response(user.serialize())
 
 @app.route("/api/users/<int:user_id>/",methods = ["DELETE"])
@@ -73,13 +74,13 @@ def delete_user(user_id):
     Endpoint for deleting a course by id
     """
     user = User.query.filter_by(id=user_id).first()
-    if User is None:
-        return failure_response("Course not found")
+    if user is None:
+        return failure_response("Course not found", 404)
     db.session.delete(user)
     db.session.commit()
     return success_response(user.serialize())
 
-@app.route("/api/reviews/",methods = ["POST"])
+@app.route("/api/users/<int:user_id>/reviews/",methods = ["POST"])
 def create_review():
     """
     Endpoint for creating a user
@@ -91,51 +92,216 @@ def create_review():
     sender = User.query.filter_by(id = the_sender_id).first()
     receiver = User.query.filter_by(id = the_receiver_id).first()
     if body.get("description") is None:
-        return json.dumps({"error":"Missing Description"}),400
+        return failure_response("Missing description")
     if body.get("sender_id") is None:
-        return json.dumps({"error":"User not found"}),400
+        return failure_response("User not found", 404)
     new_review = Review(
         sender_id = the_sender_id,
         receiver_id = the_receiver_id,
         description = description2
     )
     if receiver is None:
-        return failure_response("Receiver not found"),400
+        return failure_response("Receiver not found", 404)
     if new_review is None:
-        return failure_response("Review not made"),400
+        return failure_response("Review not made")
     sender.reviews_written_by_user.append(new_review)
     receiver.reviews_written_for_user.append(new_review)
     db.session.add(new_review)
     db.session.commit()
     return success_response(new_review.simple_serialize(),201)
 
-@app.route("/api/review/<int:review_id>/")
+@app.route("/api/users/<int:user_id>/reviews/<int:review_id>/")
 def get_review(review_id):
     """
     Endpoint for getting a specific user
     """
     review = Review.query.filter_by(id=review_id).first()
     if review is None:
-        return failure_response("Review not found")
+        return failure_response("Review not found", 404)
     return success_response(review.simple_serialize())
 
 
-@app.route("/api/review/<int:review_id>/", methods = ["DELETE"])
+@app.route("/api/users/<int:user_id>/reviews/<int:review_id>/", methods = ["DELETE"])
 def delete_review(review_id):
     """
     Endpoint for getting a specific user
     """
     review = Review.query.filter_by(id=review_id).first()
     if review is None:
-        return failure_response("Review not found"),400
+        return failure_response("Review not found", 404)
     db.session.delete(review)
     db.session.commit()
     return success_response(review.simple_serialize())
 
+
+#posts
+@app.route("/api/users/<int:user_id>/posts/", methods=["POST"])
+def create_post(user_id):
+    """
+    Endpoint for creating a post by a user
+    """
+    body = json.loads(request.data)
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found", 404)
+    title = body.get("title")
+    description = body.get("description")
+    image_path = body.get("image_path")
+    if title not in body:
+        return failure_response("Title not provided")
+    if description not in body:
+        return failure_response("Description not provided")
     
+    new_post = Post(
+        title = title,
+        description = description,
+        image_path = image_path,
+        user_id = user_id
+    )
+    db.session.add(new_post)
+    db.session.commit()
+    return success_response(new_post.new_serialize())
 
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/")
+def get_post(user_id,post_id):
+    """
+    Endpoint for getting a specific post by a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found",404)
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found", 404)
+    #if there are no comments under the post, return an empty list for 
+    #comments instead
+    try:
+        return success_response(post.serialize())
+    except:
+        return success_response(post.new_serialize())
+    
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/", methods=["POST"])
+def update_post(user_id,post_id):
+    body = json.loads(request.data)
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found",404)
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found", 404)
+    post.title = body.get("title", post.title)
+    post.description = body.get("description", post.description)
+    post.image_path = body.get("image_path", post.image_path)
 
+    db.session.commit()
+    try:
+        return success_response(post.serialize())
+    except:
+        return success_response(post.new_serialize())
 
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/", methods = ["DELETE"])
+def delete_review(user_id,post_id):
+    """
+    Endpoint for getting a specific user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found")
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found")
+    db.session.delete(post)
+    db.session.commit()
+    
+    try:
+        return success_response(post.serialize())
+    except:
+        return success_response(post.new_serialize())
+    
+#comments
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/comments/", methods=["POST"])
+def create_comment(user_id,post_id):
+    """
+    Endpoint for creating a comment
+    """
+    body = json.loads(request.data)
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found", 404)
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found", 404)
+    description = body.get("description")
+    author_id = body.get("author_id", "")
+    if author_id not in body:
+        return failure_response("Author not provided")
+    if description not in body:
+        return failure_response("Description not provided")
+    new_comment = Comment(
+        description = description,
+        author_id = author_id,
+        post_id = post_id
+    )
+    db.session.add(new_comment)
+    db.session.commit()
+    return success_response(new_comment.simple_post_serialize())
+
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/comments/<int:comment_id>/")
+def get_comment(user_id,post_id,comment_id):
+    """
+    Endpoint for getting a specific comment by a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found",404)
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found", 404)
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        return failure_response("Comment not found", 404)
+    
+    return success_response(comment.simple_post_serialize())
+
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/comments/<int:comment_id>/", methods=["POST"])
+def update_comment(user_id,post_id,comment_id):
+    """
+    Endpoint for updating a specific comment by a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found",404)
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found", 404)
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        return failure_response("Comment not found", 404)
+
+    body = json.loads(request.data)
+    comment.description = body.get("description", comment.description)
+
+    db.session.commit()
+    return success_response(comment.simple_post_serialize())
+
+@app.route("/api/users/<int:user_id>/posts/<int:post_id>/comments/<int:comment_id>/", methods=["DELETE"])
+def delete_comment(user_id,post_id,comment_id):
+    """
+    Endpoint for deleting a specific comment by a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return failure_response("User not found",404)
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return failure_response("Post not found", 404)
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        return failure_response("Comment not found", 404)
+    
+    db.session.delete(comment)
+    db.session.commit()
+    return success_response(comment.simple_post_serialize())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- db.py
+++ db.py
@@ -17,6 +17,128 @@ reviews_for_user_table = db.Table(
     db.Column("user_id",db.Integer,db.ForeignKey("user.id"))
 )
 
+class Post(db.Model):
+    """
+    Post model
+    """
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = False)
+    image_path = db.Column(db.String, nullable = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User", back_populates = "posts")
+    comments = db.relationship("Comment", back_populates = "comments")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a post object
+        """
+        self.title = kwargs.get("title", "")
+        self.description = kwargs.get("description", "")
+        self.image_path = kwargs.get("image_path", "")
+        self.user_id = kwargs.get("user_id", "")
+    
+    def serialize(self):
+        """
+        Serialize a post object
+        """
+        return {
+            "id":self.id,
+            "title":self.title,
+            "description":self.description,
+            "image_path":self.image_path,
+            "user_id":self.user_id,
+            "user":[u.simple_serialize() for u in self.user],
+            "comments":[c.simple_post_serialize() for c in self.comments]
+
+        }
+    
+    def new_serialize(self):
+        """
+        Serialize a new post object with no comments
+        """
+        return {
+            "id":self.id,
+            "title":self.title,
+            "description":self.description,
+            "image_path":self.image_path,
+            "user_id":self.user_id,
+            "user":[u.simple_serialize() for u in self.user],
+            "comments":[]
+
+        }
+    
+    def simple_serialize(self):
+        """
+        Simply serialize a post object
+        """
+        return {
+            "id":self.id,
+            "title":self.title,
+            "description":self.description,
+            "image_path":self.image_path,
+            "user_id":self.user_id,
+        }
+
+class Comment(db.Model):
+    """
+    Comment model
+    """
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    description = db.Column(db.String, nullable = False)
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    author = db.relationship("User", back_populates = "comments")
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    post = db.relationship("Post", back_populates = "comments")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a comment object
+        """
+        self.description = kwargs.get("description", "")
+        self.author_id = kwargs.get("author_id", "")
+        self.post_id = kwargs.get("post_id", "")
+
+    def serialize(self):
+        """
+        Serialize comment object
+        """
+        return {
+            "id":self.id,
+            "description":self.description,
+            "author_id":self.author_id,
+            "author":[u.simple_serialize() for u in self.author],
+            "post_id":self.post_id,
+            "post":[p.simple_serialize() for p in self.post]
+        }
+    
+    def simple_post_serialize(self):
+        """
+        Simply serialize a comment object (no posts to start loop)
+        """
+        return {
+            "id":self.id,
+            "description":self.description,
+            "author_id":self.author_id,
+            "author":[u.simple_serialize() for u in self.author],
+        }
+    
+    def simple_user_serialize(self):
+        """
+        Simply serialize a comment object (no users to start loop)
+        """
+        return {
+            "id":self.id,
+            "description":self.description,
+            "user_id":self.author_id,
+            "post_id":self.post_id,
+            "post":[p.simple_serialize() for p in self.post]
+        }
+    
+
+
 
 class User(db.Model):
     """
@@ -29,6 +151,8 @@ class User(db.Model):
     contacts = db.Column(db.String,nullable = False)
     reviews_written_by_user = db.relationship("Review",secondary = reviews_from_user_table,back_populates = "users_reviews")
     reviews_written_for_user = db.relationship("Review",secondary = reviews_for_user_table,back_populates = "users_reviews_receive")
+    posts = db.relationship("Post", back_populates = "user")
+    comments = db.relationship("Comment", back_populates = "user")
     
     def __init__(self, **kwargs):
         """
@@ -48,7 +172,9 @@ class User(db.Model):
             "bio":self.bio,
             "contacts":self.contacts,
             "reviews":[r.simple_serialize() for r in self.reviews_written_by_user],
-            "received_reviews":[a.simple_serialize() for a in self.reviews_written_for_user]
+            "received_reviews":[a.simple_serialize() for a in self.reviews_written_for_user],
+            "posts":[p.simple_serialize() for p in self.posts],
+            "comments":[c.simple_user_serialize() for c in self.comments] 
         }
         
     
